62aaa10ba439ebc1fc708a885896b62208fd3a35
MarcLudwigCareer <mdhcareer@gmail.com>
2014-11-10 21:09:32 +0000

    UPdate to use generic collection iterator

--- patterns/src/main/java/uk/co/theludwigs/actions/Outputter.java
+++ patterns/src/main/java/uk/co/theludwigs/actions/Outputter.java
@@ -1,15 +1,12 @@
 package uk.co.theludwigs.actions;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 public class Outputter {
 
-	public void showCollection (Collection collection) {
-		Iterator ii = collection.iterator();
-	   	for(int kk = 0; kk < collection.size(); kk++) {
-	                 MyClass one = (MyClass) ii.next();
-			one.printResult();
+	public void showCollection (Collection<MyClass> collection) {
+		for (MyClass myClass : collection) {
+			myClass.printResult();
 		}
 	}
 
