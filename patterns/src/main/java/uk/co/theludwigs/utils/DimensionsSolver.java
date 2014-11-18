f251be70ca7c320ca170e984926f0c51bbff982b
MarcLudwigCareer <mdhcareer@gmail.com>
2014-11-18 23:24:12 +0000

    Rename

--- patterns/src/main/java/uk/co/theludwigs/utils/DimensionsSolver.java
+++ patterns/src/main/java/uk/co/theludwigs/utils/DimensionsSolver.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 import com.google.common.collect.Lists;
 
-public class Board {
+public class DimensionsSolver {
 
 	private final int numberOfRows;
 	private final int numberOfColumns;
 	private final ArrayList<ArrayList<String>> solutions;
 	
-	public Board(final int numberOfRows, final int numberOfColumns) {
+	public DimensionsSolver(final int numberOfRows, final int numberOfColumns) {
 		this.numberOfRows = numberOfRows;
 		this.numberOfColumns = numberOfColumns;
 		this.solutions = Lists.newArrayList();
 	}
 
-	public ArrayList<ArrayList<String>> numberOfQueens() {
+	public ArrayList<ArrayList<String>> calculate() {
 		ArrayList<String> board = Lists.newArrayList();
 		board.add("Q");
 		this.solutions.add(board);
